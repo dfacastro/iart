9d8b20d59f37081ac7af07361412b744b2857d14
Diogo Castro <b.boy.souljah@gmail.com>
2011-04-14 15:04:50 +0100

    adicionados atributos do node

--- src/iart/Node.java
+++ src/iart/Node.java
@@ -10,5 +10,12 @@ package iart;
  * @author Diogo
  */
 public class Node {
+    BusStop bstop = null;
+    String bus = new String();  //autocarro usado para viajar até ao nó actual
+
+    Node(BusStop bs, String busName) {
+        bstop = bs;
+        bus = busName;
+    }
 
 }
